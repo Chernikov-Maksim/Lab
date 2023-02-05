2969da4ca992e210440b3010c20d2c41e7bc0914
Maksim-Chernikov <chernikov-max13@mail.ru>
2023-02-05 18:18:29 +0300

    opjpjp

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const burger = document.querySelector(".burger");
+
+function showMenu() {
+  burger.onсlick = function () {
+    burger.classList.toggle("burgerActive");
+  };
+}
+
+showMenu();
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
