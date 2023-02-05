5f46be1aef0d91b26c62ced6eee645f06c636fbd
Maksim-Chernikov <chernikov-max13@mail.ru>
2023-02-05 20:32:57 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ var swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
-    delay: 2000,
+    delay: 2500,
     disableOnInteraction: false,
   },
   pagination: {
